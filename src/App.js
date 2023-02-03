81009944d754a0a72a4204882d310c41a1cdb72c
Koushik Dey <koushik.dey@live.com>
2023-02-03 11:27:03 +0200

    img

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
 import ProfileCard from './ProfileCard'
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 const App = () => {
   return (
@@ -8,15 +10,15 @@ const App = () => {
         <h1>Personal Digital Assistants</h1>
       </div>
       <div>
-        <img src="https://i.ibb.co/QNfqQp8/reindeer.jpg" alt="re" />
-        <ProfileCard title="Alex" handle="@alexa99" />
+        <img src={AlexaImage} alt="re" />
+        <ProfileCard title="Alexa" handle="@alexa99" />
       </div>
       <div>
-        <img src="https://i.ibb.co/QNfqQp8/reindeer.jpg" alt="re" />
+        <img src={CortanaImage} alt="re" />
         <ProfileCard title="Crotana" handle="@crotana32" />
       </div>
       <div>
-        <img src="https://i.ibb.co/QNfqQp8/reindeer.jpg" alt="re" />
+        <img src={SiriImage} alt="re" />
         <ProfileCard title="Siri" handle="@siri01" />
       </div>
     </div>
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const ProfileCard = (props) => {
-
+const ProfileCard = ({ title, handle, image } ) => {
+  /*
     const title = props.title;
     const handle = props.handle;
     const image = props.image;
+  */
+   //  or in one line or in the arguments in the function
+    // const { title, handle, image } = props;
 
   return (
     <div>
