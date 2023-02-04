e097b0f52094215ed79ce6b740776351918a0f84
Koushik Dey <koushik.dey@live.com>
2023-02-04 16:49:34 +0200

    heart

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,33 @@
-const AnimalShow = ({type}) => {
+import { useState } from 'react';
+import bird from './images/bird.svg';
+import cat from './images/cat.svg';
+import cow from './images/cow.svg';
+import dog from './images/dog.svg';
+import gator from './images/gator.svg';
+import horse from './images/horse.svg';
+import heart from './images/heart.svg';
+
+const svgMap = {
+  // shortcut of bird:bird, cat: cat....and so on
+  bird,
+  cow,
+  cat,
+  dog,
+  gator,
+  horse
+}
+
+const AnimalShow = ({ type }) => {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
   return (
-    <div>
-      {type}
+    <div onClick={ handleClick }>
+      <img alt="animal" src={ svgMap[type] } />
+      <img alt="heart" src={ heart } />
     </div>
   )
 }
